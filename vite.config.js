50e8513726b538b7643c7fd6af0931b7e21e284f
Hugo Ladeira de Freitas <hugo_ladeira@hotmail.com>
2025-11-07 18:04:38 -0300

    chore: fix vite config build

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,32 @@
-import { defineConfig } from 'vite';
-import commonjs from 'vite-plugin-commonjs';
-import react from '@vitejs/plugin-react-swc';
-import svgr from 'vite-plugin-svgr';
-import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite'
+import commonjs from 'vite-plugin-commonjs'
+import react from '@vitejs/plugin-react-swc'
+import svgr from 'vite-plugin-svgr'
+import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
-    base: '/',
-    plugins: [commonjs(), react(), svgr()],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url))
-        }
-    },
-    build: {
-        outDir: 'build',
-        assetsDir: 'assets',
-        sourcemap: true,
-    },
-    server: {
-        port: 3000,
-        host: true,
-        open: true
-    },
-    test: {
-        globals: true,
-        environment: 'jsdom',
-        setupFiles: './src/setupTests.tsx',
-        css: true,
-        reporters: ['verbose'],
-    },
-});
+  base: '/',
+  plugins: [commonjs(), react(), svgr()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true
+  },
+  server: {
+    port: 3000,
+    host: true,
+    open: true
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.tsx',
+    css: true,
+    reporters: ['verbose']
+  }
+})
